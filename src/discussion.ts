fc61fa208ed3090e8a8e8a4cf1ac8d75c7f3a211
sectur1 <sethsamuel03@gmail.com>
2021-04-21 09:09:26 +0100

    discusion updated with new docs

--- src/discussion.ts
+++ src/discussion.ts
@@ -1,54 +1,27 @@
 /**
 * @description Github Graphql Topic
-* @defaultVariables
-* id
-* name
-* RelatedTopics
-* stargazerCount
-* Stargazers
-* viewerHasStarred
+* @fields
+** id
+** name
+** RelatedTopics
+** stargazerCount
+** Stargazers
+** viewerHasStarred
 */
 
-export const Topic = (field: string) => `
-    ${field}
+export const Topic = (fields: string) => `
+    ${fields}
 `
 
 /**
 * @description Github Graphql RelatedTopics
-* @defaultVariables
-* first number
-* field Topic
+* @defaultVariables first number
+* @fields
+** Topic
 */
 
-export const RelatedTopics = (first: number = 10, field: string) => `
+export const RelatedTopics = (first: number = 10, fields: string) => `
     relatedTopics(first: ${first}) {
-        ${field}
-    }
-`
-
-/**
-* @description Github Graphql Stargazers
-* @defaultVariables
-* first number
-* last number
-* after string
-* before string
-* orderBy "STARRED_AT"
-* direction "ASC" | "DESC"
-* field User
-*/
-
-export const Stargazers = (first: number = 10, field: string, pageInfo?: string, orderBy: string = 'STARRED_AT', direction: string = 'ASC', after?: string, before?: string, last?: string) => `
-    stargazers(first: ${first} ${after ? `, ${after}` : ''} ${before ? `, ${before}` : ''} ${last ? `, ${last}` : ''}, orderBy: {field: ${orderBy}, direction: ${direction}}) {
-        edges {
-            cursor
-            node {
-                ${field}
-            }
-            starredAt
-        }
-        nodes {${field}}
-        ${pageInfo || ''}
-        totalCount
+        ${fields}
     }
 `
