0eff0173c65bb27a0f31db541e640efece331f02
ioedeveloper <daviddisu8@gmail.com>
2021-05-30 22:13:05 +0100

    Request queries and include nodes

--- src/builder.ts
+++ src/builder.ts
@@ -1,26 +1,55 @@
-import { onUser } from './nodes'
+import { onUser, onProject } from './nodes'
+import { User, Project } from './queries'
 
+interface INode {
+  user: (...args: any) => string,
+  project: (...args: any) => string
+}
+
+interface IQuery {
+  user: (...args: any) => string,
+  project: (...args: any) => string
+}
 export default class QueryBuilder {
   private _query: string
-  private _queryTitleMap: Record<string, (...args: any) => string> = {
-    user: onUser
+  private _nodeTitleMap: INode = {
+    user: onUser,
+    project: onProject
   }
 
-  on (node: string, params: Record<string, any>) {
-    this._query.replace(node, this._queryTitleMap[node](params.fields))
+  private _queryTitleMap: IQuery = {
+    user: User,
+    project: Project
+  }
+
+  on (node: keyof INode, params: Record<string, string | number>) {
+    const args = Object.entries(params).map(([, val]) => val)
+    const nodeFragment = this._nodeTitleMap[node](...args)
+    const regex = new RegExp(node, 'g')
+
+    this._query = this._query.replace(regex, nodeFragment)
     return this
   }
 
   get query () {
     return this._query
   }
+
+  request (query: keyof IQuery, params: Record<string, string | number>) {
+    const args = Object.entries(params).map(([, val]) => val)
+
+    this._query = this._queryTitleMap[query](...args)
+    return this
+  }
 }
 
 const QB = new QueryBuilder()
 
-const query = QB.on('users', {
-  fields: 'id, email, age, projects',
-  first: 10
-}).on('projects', {
-  fields: 'name, dateCreated'
+const query = QB.request('user', {
+  fields: 'id email age project',
+  login: 'ioedeveloper'
+}).on('project', {
+  fields: 'name dateCreated'
 }).query
+
+console.log('query: ', query)
